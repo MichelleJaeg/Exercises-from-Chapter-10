2b6d17be8ddcf7ce2fa30e5b21837753a3fa7787
MichelleJaeg <michelles82@gmail.com>
2013-11-18 16:37:34 -0800

    Added spaces to make code more readable and slight changes to a couple variable names

--- 334.4.py
+++ 334.4.py
@@ -5,72 +5,71 @@ from button import Button
 from random import randrange
 
 
-
 def main ():
 
     #Create graphics window
-    win=GraphWin(400,400)
-    win.setCoords(0.0,0.0,10.0,10.0)
+    win = GraphWin(400, 400)
+    win.setCoords(0.0, 0.0, 10.0, 10.0)
 
     #Create Message
-    message1=Text(Point(5.0, 9.0),'Welcome to Three Button Monte')
+    message1 = Text(Point(5.0, 9.0),'Welcome to Three Button Monte')
     message1.draw(win)
 
     #Create buttons
-    number1=Button(win, Point(2.0,4.0),1.0,1.0,'Door 1')
+    number1 = Button(win, Point(2.0, 4.0),1.0, 1.0,'Door 1')
     number1.activate()
-    number2=Button(win, Point(5.0,4.0),1.0, 1.0, 'Door 2')
+    number2 = Button(win, Point(5.0, 4.0),1.0, 1.0, 'Door 2')
     number2.activate()
-    number3=Button(win, Point(8.0,4.0),1.0, 1.0, 'Door 3')
+    number3 = Button(win, Point(8.0, 4.0), 1.0, 1.0, 'Door 3')
     number3.activate()
 
     #Create quit button
-    quitbutton=Button(win, Point(2.0, 1.0), 2.0, 2.0,'Click to Quit')
-    quitbutton.activate()
+    quit_button = Button(win, Point(2.0, 1.0), 2.0, 2.0,'Click to Quit')
+    quit_button.activate()
 
     #Ask user to pick a door
-    message2=Text(Point(5.0,7.0),'A door has been chosen. Click on a door to guess it. ')
+    message2 = Text(Point(5.0, 7.0),'A door has been chosen. Click on a door to guess it. ')
     message2.draw(win)
-    pt=win.getMouse()
+    pt = win.getMouse()
 
     #While user has not chosen to quit
-    counter=0
-    while not quitbutton.clicked(pt):
+    counter = 0
+    while not quit_button.clicked(pt):
 
-        if counter!=0:
+        if counter != 0:
 
     #Ask user to pick a door
             message2.setText('Click on another door. ')
-            pt=win.getMouse()
+            pt = win.getMouse()
 
     #Report results to user
-        x=pickadoor()
-        if x==1 and number1.clicked(pt)==True:
+        x = pickADoor()
+        if x == 1 and number1.clicked(pt) == True:
             message1.setText('You picked the correct door! ')
-        elif x==2 and number2.clicked(pt)==True:
+        elif x == 2 and number2.clicked(pt) == True:
             message1.setText('You picked the correct door! ')
-        elif x==3 and number3.clicked(pt)==True:
+        elif x == 3 and number3.clicked(pt) == True:
             message1.setText('You picked the correct door! ')
         else:
-            if x==1:
+            if x == 1:
                 message1.setText("You did not pick the correct door. The correct door was 1")
-            if x==2:
+            if x == 2:
                 message1.setText("You did not pick the correct door. The correct door was 2")
-            if x==3:
+            if x == 3:
                 message1.setText("You did not pick the correct door. The correct door was 3")
-        counter+=1
+        counter += 1
 
     #Quit program
     win.close()
 
-def pickadoor():
-    doornumber=randrange(1,4)
-    if doornumber==1:
-        door=1
-    if doornumber==2:
-        door=2
-    if doornumber==3:
-        door=3
+def pickADoor():
+    door_number = randrange(1, 4)
+    if door_number == 1:
+        door = 1
+    if door_number == 2:
+        door = 2
+    if door_number == 3:
+        door = 3
     return door
 
 
@@ -78,5 +77,5 @@ def pickadoor():
 
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main ()
