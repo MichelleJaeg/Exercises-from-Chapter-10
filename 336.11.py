ddd0b0f2eeb87cd330908f62be68dfc8ac7d80cc
MichelleJaeg <michelles82@gmail.com>
2013-11-20 15:24:50 -0800

    Added spaces to make code more readable

--- 336.11.py
+++ 336.11.py
@@ -8,17 +8,17 @@ def main ():
      print ("This program prints out n randomly generated playing cards. ")
 
      #Get input
-     n=eval(input("\nHow many cards would you like? "))
+     n = eval(input("\nHow many cards would you like? "))
 
      #Generate cards
      for i in range(n):
-         rank=random.randrange(1,13)
-         suit=random.choice(["s","h", "c", "d"])
-         card=Card(rank,suit)
+         rank = random.randrange(1, 13)
+         suit = random.choice(["s", "h", "c", "d"])
+         card = Card(rank, suit)
          print ()
          card.__str__()
          print ("The blackjack value of the card is", card.BJValue())
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main ()
