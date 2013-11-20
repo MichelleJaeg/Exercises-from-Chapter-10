89958ca069031647dc241509df3c5d5dd369d456
MichelleJaeg <michelles82@gmail.com>
2013-11-19 19:58:51 -0800

    Added spaces to make code more readable and slight change to a variable name

--- 334.6.py
+++ 334.6.py
@@ -8,24 +8,24 @@ def main ():
     print ()
 
     #Create student object
-    astudent=Student("student", 0,0)
+    a_student = Student("student", 0, 0)
 
     #User enters information for each course
-    keepgoing=input("Type any key to enter another course or type enter to quit. ")
-    while keepgoing!='':
-        credits=eval(input("Please enter the number of credits in the course. "))
-        letterGrade=input("Please enter the letter grade earned by the student. ")
-        astudent.addLetterGrade(letterGrade,credits)
-        keepgoing=input("Type any key to enter another course or type enter to quit. ")
+    keepgoing = input("Type any key to enter another course or type enter to quit. ")
+    while keepgoing != '':
+        credits = eval(input("Please enter the number of credits in the course. "))
+        letterGrade = input("Please enter the letter grade earned by the student. ")
+        a_student.addLetterGrade(letterGrade, credits)
+        keepgoing = input("Type any key to enter another course or type enter to quit. ")
         print ()
 
     #Print results
-    GPA=astudent.gpa()
-    print ("The student's GPA is: ",GPA)
+    GPA = a_student.gpa()
+    print ("The student's GPA is: ", GPA)
 
 
 
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main ()
