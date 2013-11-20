1d8196599e03b550aea14c720dd24f3057d850f8
MichelleJaeg <michelles82@gmail.com>
2013-11-20 15:32:25 -0800

    Added spaces to make code more readable

--- 338.16.py
+++ 338.16.py
@@ -2,26 +2,24 @@
 
 from projectile import Projectile
 from tracker import Tracker
-from graphics import*
+from graphics import *
 from time import sleep
 sleep(0.005)
 from target import Target
 
 
-
 def main ():
-
 
     #Introduction
     print ("This program uses a cannonball to shoot at a target.")
 
     #Create graphics window
-    win=GraphWin(200,200)
-    win.setCoords(0.0,0.0,25.0,25.0)
-    target=Target(win)
+    win = GraphWin(200, 200)
+    win.setCoords(0.0, 0.0, 25.0, 25.0)
+    target = Target(win)
 
-    flag=False
-    targetHit=False
+    flag = False
+    targetHit = False
     while not flag:
 
     #Get inputs
@@ -31,31 +29,31 @@ def main ():
         h = eval(input("Enter the initial height in meters: "))
 
     #Create tracker
-        projectile=Projectile(a,v,h)
-        tracker=Tracker(win, projectile)
-        time=0.0
-        while projectile.getY()>=-5:
-            time+=.0005
+        projectile = Projectile(a, v, h)
+        tracker = Tracker(win, projectile)
+        time = 0.0
+        while projectile.getY() >= -5:
+            time += .0005
             projectile.update(time)
             tracker.update()
 
     #Calculate if cannonball hit target
-            points=target.points()
-            center=tracker.circ.getCenter()
-            center_x=center.getX()
-            center_y=center.getY()
-            radius=tracker.circ.getRadius()
+            points = target.points()
+            center = tracker.circ.getCenter()
+            center_x = center.getX()
+            center_y = center.getY()
+            radius = tracker.circ.getRadius()
             for point in points:
-                x=point.getX()
-                y=point.getY()
-                square_dist = (center_x-x) **2 + (center_y-y) **2
-                if square_dist<=radius**2:
-                    targetHit=True
+                x = point.getX()
+                y = point.getY()
+                square_dist = (center_x-x) ** 2 + (center_y-y) ** 2
+                if square_dist <= radius ** 2:
+                    targetHit = True
         if targetHit:
             print ("\nYou hit the target!")
-            flag=True
+            flag = True
         else:
-            flag=False
+            flag = False
             print ("\nTry again!")
 
 
@@ -68,5 +66,5 @@ def main ():
 
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main ()
