d36ca8ec41a73df7174ccb1df069cfc93d324d6d
MichelleJaeg <michelles82@gmail.com>
2013-11-19 20:31:50 -0800

    Added spaces to make code more readable and slight change to a variable name

--- 335.10.py
+++ 335.10.py
@@ -1,30 +1,30 @@
 # Problem 10
-from graphics import*
+from graphics import *
 
 class Cube:
 
-    def __init__(self, sidelength):
-        self.sidelength=sidelength
+    def __init__(self, side_length):
+        self.side_length = side_length
 
     def getSideLength(self):
-        return self.sidelength
+        return self.side_length
 
     def surfaceArea(self):
-        return 6 * self.sidelength**2
+        return 6 * self.side_length ** 2
 
 
     def volume(self):
-        return self.sidelength**3
+        return self.side_length ** 3
 
 def main ():
     #Introduction
     print ("This program calculates the volume and surface area of a cube. ")
 
     #Get input
-    sidelength=eval(input("\nPlease enter the length of a side of the cube. "))
+    side_length = eval(input("\nPlease enter the length of a side of the cube. "))
 
     #Create sphere
-    cube=Cube(sidelength)
+    cube = Cube(side_length)
 
     #Print results
     print ("\nThe volume of the cube is:", round(cube.volume()))
